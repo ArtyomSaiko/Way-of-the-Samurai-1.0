b541c9182c36dded32a4d52c02fe921cd60f080f
Artyom <artem.sok27@gmail.com>
2022-12-10 16:14:01 +0300

    commit

---
+++ lessons 9/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <a href ='#s'> Home </a>
+            <a href ='#s'> News Feed </a>
+            <a href ='#s'> Messeges </a>
+        </div>
+    );
+}
