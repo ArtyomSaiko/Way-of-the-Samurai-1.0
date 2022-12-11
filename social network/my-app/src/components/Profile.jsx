180c35b847a66c0450f9b74e95c4f632c1d3c094
Artyom <artem.sok27@gmail.com>
2022-12-11 12:19:19 +0300

    commit

---
+++ social network/my-app/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = ()=>{
+    return <div className='content'>
+    <div>
+    <img src='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/4d/45/97/minsk.jpg?w=700&h=500&s=1' />
+    </div>
+    <div>
+        ava + text
+    </div>
+    <div>
+        my post
+    </div>
+    <div>
+        new post
+    </div>
+</div>
+}
+
+export default Profile;
