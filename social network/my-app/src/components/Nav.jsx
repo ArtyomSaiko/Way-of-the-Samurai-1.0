0226f911fc980fc1041b9887caaaafb77056fd0f
Artyom <artem.sok27@gmail.com>
2022-12-11 20:54:19 +0300

    commit

--- social network/my-app/src/components/Nav.jsx
+++ social network/my-app/src/components/Nav.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import s from './Nav.module.css';
 
 const Nav = ()=>{
-    return <nav className='nav'>
-    <div>
+    return <nav className={s.nav}>
+    <div className ={s.item}>
        <a>Profil</a> 
     </div>
-    <div>
+    <div className ={s.item}>
        <a>Massages</a> 
     </div>
-    <div>
+    <div className ={s.item}>
        <a>News</a> 
     </div>
-    <div>
+    <div className ={s.item}>
        <a>Music</a> 
     </div>
-    <div>
+    <div className ={s.item}>
        <a>Settings</a> 
     </div>
 </nav>
