1ba085e6454e47526d3cc53c0fda5fd679d87829
Artyom <artem.sok27@gmail.com>
2022-12-11 12:19:12 +0300

    commit

---
+++ social network/my-app/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = ()=>{
+    return <nav className='nav'>
+    <div>
+       <a>Profil</a> 
+    </div>
+    <div>
+       <a>Massages</a> 
+    </div>
+    <div>
+       <a>News</a> 
+    </div>
+    <div>
+       <a>Music</a> 
+    </div>
+    <div>
+       <a>Settings</a> 
+    </div>
+</nav>
+}
+
+export default Nav;
