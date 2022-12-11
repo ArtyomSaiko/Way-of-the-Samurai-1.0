1e782781cbbd7755353014251ec5b22c433e44d5
Artyom <artem.sok27@gmail.com>
2022-12-11 21:23:26 +0300

    two class

--- social network/my-app/src/components/Nav.jsx
+++ social network/my-app/src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from './Nav.module.css';
-
+//  <div className ={ `${s.item} ${s.active}`}> если у нас нужно 2 класса применить
 const Nav = ()=>{
     return <nav className={s.nav}>
     <div className ={s.item}>
        <a>Profil</a> 
     </div>
-    <div className ={s.item}>
+    <div className ={ `${s.item} ${s.active}`}>
        <a>Massages</a> 
     </div>
     <div className ={s.item}>
