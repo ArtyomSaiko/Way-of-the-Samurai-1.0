93ea4ea985d9aa88606e81fb96f7ebcaacd586d7
Artyom <artem.sok27@gmail.com>
2022-12-11 20:53:48 +0300

    commit

--- social network/my-app/src/components/Header.jsx
+++ social network/my-app/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = ()=>{
-    return <header className='header'>
-    <img src='https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn' />
+    return <header className={s.header}>
+    <img src='https://w-dog.ru/wallpapers/15/5/532183089661031/adidas-adidas-originaly-logotip-fon-trava.jpg' />
 </header>
 }
 
