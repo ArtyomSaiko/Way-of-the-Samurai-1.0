ffdc5b4aa62e31f799a18755817096a489222e19
Artyom <artem.sok27@gmail.com>
2022-12-11 12:19:05 +0300

    commit

---
+++ social network/my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ()=>{
+    return <header className='header'>
+    <img src='https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn' />
+</header>
+}
+
+export default Header;
