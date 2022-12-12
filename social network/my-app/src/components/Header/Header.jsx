b7a42b54173da5155f6e1432e716d614b14d5861
Artyom <artem.sok27@gmail.com>
2022-12-12 21:55:37 +0300

    commit

---
+++ social network/my-app/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = ()=>{
+    return <header className={s.header}>
+    <img src='https://w-dog.ru/wallpapers/15/5/532183089661031/adidas-adidas-originaly-logotip-fon-trava.jpg' />
+</header>
+}
+
+export default Header;
