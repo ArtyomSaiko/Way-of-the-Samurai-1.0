5aad2289adf0dfd7f68adf4f263fe840c27bd826
Artyom <artem.sok27@gmail.com>
2022-12-12 22:36:24 +0300

    commit

---
+++ social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Posts from './Posts/Posts';
+
+const MyPosts = () => {
+  return (
+    <div>
+      My posts
+      <div>
+        <textarea></textarea>
+        <button>add post</button>
+      </div>
+      <div className={s.posts}>
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+        <Posts />
+      </div>
+    </div>)
+}
+
+export default MyPosts;
