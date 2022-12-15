42c98826fdce562892f699ebfcc9f88ab46980c2
Artyom <artem.sok27@gmail.com>
2022-12-16 00:04:49 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs = (props) => {
-    return (<div>
-        Dialogs
-    </div>
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.messages}>
+                    <div className={s.dialog}>Hi</div>
+                    <div className={s.dialog}>How is you it-kamasytra</div>
+                    <div className={s.dialog}>You</div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Dialogs;
