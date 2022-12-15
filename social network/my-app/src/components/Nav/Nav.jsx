155e26923cb426ae8f8b22df19865a48bc28cf35
Artyom <artem.sok27@gmail.com>
2022-12-15 22:06:22 +0300

    commit

--- social network/my-app/src/components/Nav/Nav.jsx
+++ social network/my-app/src/components/Nav/Nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './Nav.module.css';
-//  <div className ={ `${s.item} ${s.active}`}> если у нас нужно 2 класса применить
+
 const Nav = ()=>{
     return <nav className={s.nav}>
     <div className ={s.item}>
-       <a>Profil</a> 
+       <a href='/profile'>Profil</a> 
     </div>
     <div className ={ `${s.item} ${s.active}`}>
-       <a>Massages</a> 
+       <a href='/dialogs'>Massages</a> 
     </div>
     <div className ={s.item}>
        <a>News</a> 
