9adc7d0e6c64a608a03b79f4a9f4e6726be409bb
Artyom <artem.sok27@gmail.com>
2022-12-15 20:52:58 +0300

    commit

--- social network/my-app/src/components/Profile/Profile.jsx
+++ social network/my-app/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts.jsx';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return (<div className={s.content}>
+    return (<div>
         <div>
             <img src='https://zastavok.net/main/auto/1423524421.jpg' />
         </div>
