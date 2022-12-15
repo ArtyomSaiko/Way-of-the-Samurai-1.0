6183d9bd4ec34adba6270b49b445652f411df3b4
Artyom <artem.sok27@gmail.com>
2022-12-15 22:06:29 +0300

    commit

--- social network/my-app/src/App.js
+++ social network/my-app/src/App.js
@@ -4,17 +4,22 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Nav />
-            <div class='app-wrapper-content'>
-                <Dialogs />
-                {/*} <Profile />*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Nav />
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/dialogs" element={<Dialogs />} />
+                    </Routes>
+                </div>
             </div>
-        </div>);
+        </BrowserRouter>);
 }
 
 export default App;
