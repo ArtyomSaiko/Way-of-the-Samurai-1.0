0bcfe46a157a9cff2393a0626e3279dfa0804dc9
Artyom <artem.sok27@gmail.com>
2022-12-16 22:45:01 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs = () => {
+const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                <div className={s.dialog}>
-                    Andrey
-                </div>
-                <div className={s.dialog}>
-                    Sveta
-                </div>
-                <div className={s.dialog}>
-                    Sasha
-                </div>
-                <div className={s.dialog}>
-                    Dimych
-                </div>
-                <div className={s.dialog}>
-                    Victor
-                </div>
-                <div className={s.dialog}>
-                    Valera
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>
+                        Andrey
+                    </div>
+                    <div className={s.dialog}>
+                        Sveta
+                    </div>
+                    <div className={s.dialog}>
+                        Sasha
+                    </div>
+                    <div className={s.dialog}>
+                        Dimych
+                    </div>
+                    <div className={s.dialog}>
+                        Victor
+                    </div>
+                    <div className={s.dialog}>
+                        Valera
+                    </div>
                 </div>
                 <div className={s.messages}>
                     <div className={s.dialog}>Hi</div>
-                    <div className={s.dialog}>How is you it-kamasytra</div>
+                    <div className={s.dialog}>How is you</div>
                     <div className={s.dialog}>You</div>
                 </div>
             </div>
