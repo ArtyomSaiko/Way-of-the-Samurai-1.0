270fdd145fedf964c1e47ca04c4da9d8ac31922d
Artyom <artem.sok27@gmail.com>
2022-12-17 21:40:28 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to="/dialogs/1"> Andrey </NavLink>
+        </div>
+    )
+}
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <div className={s.dialog + ' ' + s.active}>
-                        <NavLink to="/dialogs/1"> Andrey </NavLink>
-                    </div>
+                    <DialogItem />
                     <div className={s.dialog}>
                         <NavLink to="/dialogs/2">Sveta</NavLink>
                     </div>
