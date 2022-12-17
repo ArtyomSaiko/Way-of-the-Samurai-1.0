ad9798dc126a71c67afbe03472e043fa6d36d627
Artyom <artem.sok27@gmail.com>
2022-12-17 21:08:02 +0300

    NavLink

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
@@ -6,23 +7,23 @@ const Dialogs = (props) => {
         <div className={s.dialogs}>
             <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <div className={s.dialog}>
-                        Andrey
+                    <div className={s.dialog + ' ' + s.active}>
+                        <NavLink to="/dialog/1"> Andrey </NavLink>
                     </div>
                     <div className={s.dialog}>
-                        Sveta
+                        <NavLink to="/dialog/2">Sveta</NavLink>
                     </div>
                     <div className={s.dialog}>
-                        Sasha
+                        <NavLink to="/dialog/3">Sasha</NavLink>
                     </div>
                     <div className={s.dialog}>
-                        Dimych
+                        <NavLink to="/dialog/4">Dimych</NavLink>
                     </div>
                     <div className={s.dialog}>
-                        Victor
+                        <NavLink to="/dialog/5">Victor</NavLink>
                     </div>
                     <div className={s.dialog}>
-                        Valera
+                        <NavLink to="/dialog/6">Valera</NavLink>
                     </div>
                 </div>
                 <div className={s.messages}>
