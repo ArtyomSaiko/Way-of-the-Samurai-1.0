88d09c691225cf354e3566176413448a29ecb942
Artyom <artem.sok27@gmail.com>
2022-12-17 22:16:45 +0300

    commit

---
+++ social network/my-app/src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+    return (
+    <div>
+        <div>
+            <img src='https://zastavok.net/main/auto/1423524421.jpg' />
+        </div>
+        <div>
+            ava + text
+        </div>
+     </div>)
+}
+
+export default ProfileInfo;
