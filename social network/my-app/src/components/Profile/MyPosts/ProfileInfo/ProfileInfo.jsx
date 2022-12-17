cb9c306bd75c3c1311cc96811dd2c713ad48e8aa
Artyom <artem.sok27@gmail.com>
2022-12-17 22:25:23 +0300

    commit

--- social network/my-app/src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ social network/my-app/src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -3,14 +3,14 @@ import s from './ProfileInfo.module.css';
 
 const ProfileInfo = () => {
     return (
-    <div>
         <div>
-            <img src='https://zastavok.net/main/auto/1423524421.jpg' />
-        </div>
-        <div>
-            ava + text
-        </div>
-     </div>)
+            <div>
+                <img src='https://zastavok.net/main/auto/1423524421.jpg' />
+            </div>
+            <div className={s.descriptionBlok}>
+                ava + text
+            </div>
+        </div>)
 }
 
 export default ProfileInfo;
