cff73b7c5332c97b092e315a522eb6e5757a3fcc
Artyom <artem.sok27@gmail.com>
2022-12-17 22:47:30 +0300

    commit

--- social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,19 @@ import Posts from './Posts/Posts';
 const MyPosts = () => {
   return (
     <div className={s.postBlock}>
-      My posts
+      <h3>My posts</h3>
       <div>
-        <textarea></textarea>
-        <button>add post</button>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>add post</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Posts message = 'He, how are you?' like = '10' />
-        <Posts message = "It's, my first post" like = '23' />
-        </div>
+        <Posts message='He, how are you?' like='10' />
+        <Posts message="It's, my first post" like='23' />
+      </div>
     </div>)
 }
 
