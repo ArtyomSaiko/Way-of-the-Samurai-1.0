790943099a4f35d8479866c9ff86e4f552c3e1e6
Artyom <artem.sok27@gmail.com>
2022-12-20 22:43:26 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -9,10 +9,27 @@ let posts = [
   { id: 2, message: 'It\s, my first post', likesCount: 23 },
 ]
 
+let dialogs = [
+  { id: 1, name: 'Andrey' },
+  { id: 2, name: 'Svetay' },
+  { id: 3, name: 'Sasha' },
+  { id: 4, name: 'Dimych' },
+  { id: 5, name: 'Victo' },
+  { id: 6, name: 'Valera' },
+]
+
+let messages = [
+  { id: 1, message: "Hi" },
+  { id: 2, message: "How is you" },
+  { id: 3, message: "Yo" },
+  { id: 4, message: "Yo" },
+  { id: 5, message: "Yo" },
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App posts={posts} />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>
 );
 
