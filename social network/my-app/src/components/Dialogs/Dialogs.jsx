d41a55a90d35f8b9ad9e9f40daf0a673e8a75bf7
Artyom <artem.sok27@gmail.com>
2022-12-20 22:43:18 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -6,26 +6,9 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: 'Andrey' },
-        { id: 2, name: 'Svetay' },
-        { id: 3, name: 'Sasha' },
-        { id: 4, name: 'Dimych' },
-        { id: 5, name: 'Victo' },
-        { id: 6, name: 'Valera' },
-    ]
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
 
-    let messages = [
-        { id: 1, message: "Hi" },
-        { id: 2, message: "How is you" },
-        { id: 3, message: "Yo" },
-        { id: 4, message: "Yo" },
-        { id: 5, message: "Yo" },
-    ]
-
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-
-    let messagesElements = messages.map(m => <Message message={m.message} id={m.id} />);
+    let messagesElements = props.messages.map(m => <Message message={m.message} id={m.id} />);
 
     return (
         <div className={s.dialogs}>
