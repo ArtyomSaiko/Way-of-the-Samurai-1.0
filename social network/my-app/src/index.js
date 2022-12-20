fd93bf939fc73ba3d1116906af0379908dd940b9
Artyom <artem.sok27@gmail.com>
2022-12-20 22:29:50 +0300

    commit

---
+++ social network/my-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+let posts = [
+  { id: 1, message: 'He, how are you?', likesCount: 10 },
+  { id: 2, message: 'It\s, my first post', likesCount: 23 },
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App posts={posts} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
