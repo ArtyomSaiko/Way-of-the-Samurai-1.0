452422828e7a59e9d253763153c3bdd150d114de
Artyom <artem.sok27@gmail.com>
2022-12-24 16:33:14 +0300

    commit

--- social network/my-app/src/components/Dialogs/Message/Message.jsx
+++ social network/my-app/src/components/Dialogs/Message/Message.jsx
@@ -3,9 +3,9 @@ import s from './../Dialogs.module.css';
 
 
 const Message = (props) => {
-    return (
-        <div className={s.dialog}>{props.message}</div>
-    )
+  return (
+    <div className={s.dialog}>{props.message}</div>
+  )
 }
 
 export default Message;
