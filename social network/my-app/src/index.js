9455e84c458f137e06f72c82a274b7016ed6c512
Artyom <artem.sok27@gmail.com>
2022-12-24 12:02:40 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -4,27 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let posts = [
-  { id: 1, message: 'He, how are you?', likesCount: 10 },
-  { id: 2, message: 'It\s, my first post', likesCount: 23 },
-]
-
-let dialogs = [
-  { id: 1, name: 'Andrey' },
-  { id: 2, name: 'Svetay' },
-  { id: 3, name: 'Sasha' },
-  { id: 4, name: 'Dimych' },
-  { id: 5, name: 'Victo' },
-  { id: 6, name: 'Valera' },
-]
-
-let messages = [
-  { id: 1, message: "Hi" },
-  { id: 2, message: "How is you" },
-  { id: 3, message: "Yo" },
-  { id: 4, message: "Yo" },
-  { id: 5, message: "Yo" },
-]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
