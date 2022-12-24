6a0e16fdb62c9c9ba1d71544b6928ae914e95cd2
Artyom <artem.sok27@gmail.com>
2022-12-24 12:16:01 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import state from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App appState={state} />
   </React.StrictMode>
 );
 
