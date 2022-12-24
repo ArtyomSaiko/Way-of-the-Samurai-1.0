341b046dbb1a4d6738fcc24a9638a230a9e63544
Artyom <artem.sok27@gmail.com>
2022-12-24 16:56:28 +0300

    addPost

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
+import {addPost} from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App state={state} />
+      <App state={state} addPost={addPost} />
     </BrowserRouter>
   </React.StrictMode>
 );
