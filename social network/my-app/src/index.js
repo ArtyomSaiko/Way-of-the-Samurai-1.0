1fc721e99cafda06611ed45309080c1a64a14902
Artyom <artem.sok27@gmail.com>
2022-12-24 12:36:50 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -8,7 +8,7 @@ import state from './redux/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App appState={state} />
+    <App state={state} />
   </React.StrictMode>
 );
 
