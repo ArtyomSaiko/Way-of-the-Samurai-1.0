764dac46425c3a1dee35780350a416fa8c840872
Artyom <artem.sok27@gmail.com>
2022-12-24 12:15:54 +0300

    commit

--- social network/my-app/src/App.js
+++ social network/my-app/src/App.js
@@ -17,8 +17,10 @@ const App = (props) => {
                 <Nav />
                 <div class='app-wrapper-content'>
                     <Routes>
-                        <Route path="/profile/*" element={<Profile posts={props.posts} />} />
-                        <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
+                        <Route path="/profile/*" 
+                        element={<Profile posts={props.appState.posts} />} />
+                        <Route path="/dialogs/*" 
+                        element={<Dialogs dialogs={props.appState.dialogs} messages={props.appState.messages} />} />
                         <Route path="/news/*" element={<News />} />
                         <Route path="/music/*" element={<Music />} />
                         <Route path="/settings/*" element={<Settings />} />
