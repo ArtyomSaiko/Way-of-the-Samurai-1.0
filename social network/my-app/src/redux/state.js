d97a18bf56330f8c9dd9106f5d884d4ef1f5338f
Artyom <artem.sok27@gmail.com>
2022-12-24 12:02:31 +0300

    commit

---
+++ social network/my-app/src/redux/state.js
@@ -0,0 +1,21 @@
+  let state = {
+    posts:[
+        { id: 1, message: 'He, how are you?', likesCount: 10 },
+        { id: 2, message: 'It\s, my first post', likesCount: 23 },
+      ],
+    dialogs:[
+        { id: 1, name: 'Andrey' },
+        { id: 2, name: 'Svetay' },
+        { id: 3, name: 'Sasha' },
+        { id: 4, name: 'Dimych' },
+        { id: 5, name: 'Victo' },
+        { id: 6, name: 'Valera' },
+      ],
+    messages:[
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How is you" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+      ]
+  }
