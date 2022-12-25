a2addf9534557fee6f2d9cddd0bd2e5304613da9
Artyom <artem.sok27@gmail.com>
2022-12-25 16:44:17 +0300

    commit

--- social network/my-app/src/components/Profile/Profile.jsx
+++ social network/my-app/src/components/Profile/Profile.jsx
@@ -8,7 +8,9 @@ const Profile = (props) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.state.posts} addPost={props.addPost} />
+            <MyPosts posts={props.profilePage.posts}
+                newPostText={props.profilePage.newPostText}
+                addPost={props.addPost} />
         </div>)
 }
 
