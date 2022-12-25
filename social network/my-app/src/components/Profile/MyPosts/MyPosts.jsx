8a485a9269808812e2e5eac8652083770083df61
Artyom <artem.sok27@gmail.com>
2022-12-25 16:07:10 +0300

    commit

--- social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,7 +11,8 @@ const MyPosts = (props) => {
 
   let addPost = () => {
     let text = newPostElement.current.value;
-    props.addPost(text)
+    props.addPost(text);
+    newPostElement.current.value = ' ';
   }
 
   return (
