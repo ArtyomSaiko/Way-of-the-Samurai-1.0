e246e0efc5eed2d48c3943cf0594c3c53ade65ae
Artyom <artem.sok27@gmail.com>
2022-12-28 22:12:12 +0300

    commit

--- social network/my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ social network/my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './../Dialogs.module.css';
 
-
 const DialogItem = (props) => {
 
   let path = "/dialogs/" + props.id;
