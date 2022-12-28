f747bb24367f96b36604f6cffd017abf4cd8510c
Artyom <artem.sok27@gmail.com>
2022-12-28 22:12:33 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -3,8 +3,6 @@ import reportWebVitals from './reportWebVitals';
 import { rerenderEntireTree } from './render';
 import state from './redux/state';
 
-
-
 rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
