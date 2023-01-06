7b8b4b364acc5a67e1c1775738ee6551a5ee25f4
Artyom <artem.sok27@gmail.com>
2023-01-06 20:46:53 +0300

    c

--- social network/my-app/src/redux/state.js
+++ social network/my-app/src/redux/state.js
@@ -43,7 +43,7 @@ export const addPost = () => {
 
 export const upDateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
+    rerenderEntireTree();
 }
 
 export const subscribe = (observer) => {
