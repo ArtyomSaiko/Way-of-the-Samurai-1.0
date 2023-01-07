787f48a555ced6a9f9bf4bafdc05c62076a2d0e1
Artyom <artem.sok27@gmail.com>
2023-01-07 21:57:07 +0300

    commit

--- social network/my-app/src/redux/state.js
+++ social network/my-app/src/redux/state.js
@@ -1,53 +1,55 @@
-let rerenderEntireTree = () => {
-    console.log ('State')
-};
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'He, how are you?', likesCount: 10 },
-            { id: 2, message: 'It\s, my first post', likesCount: 23 },
-        ],
-        newPostText: 'it-kamasytra'
+let store = {
+    _state : {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'He, how are you?', likesCount: 10 },
+                { id: 2, message: 'It\s, my first post', likesCount: 23 },
+            ],
+            newPostText: 'it-kamasytra'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Andrey' },
+                { id: 2, name: 'Svetay' },
+                { id: 3, name: 'Sasha' },
+                { id: 4, name: 'Dimych' },
+                { id: 5, name: 'Victo' },
+                { id: 6, name: 'Valera' },
+            ],
+            messages: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "How is you" },
+                { id: 3, message: "Yo" },
+                { id: 4, message: "Yo" },
+                { id: 5, message: "Yo" },
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Andrey' },
-            { id: 2, name: 'Svetay' },
-            { id: 3, name: 'Sasha' },
-            { id: 4, name: 'Dimych' },
-            { id: 5, name: 'Victo' },
-            { id: 6, name: 'Valera' },
-        ],
-        messages: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "How is you" },
-            { id: 3, message: "Yo" },
-            { id: 4, message: "Yo" },
-            { id: 5, message: "Yo" },
-        ]
+    getState(){
+        return _state;
+    },
+    rerenderEntireTree () {
+        console.log ('State')
+    },
+    addPost () {
+        let newPost = {
+            id: 5,
+            message: state.profilePage.newPostText,
+            likesCount: 0,
+        };
+    
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = '';
+        rerenderEntireTree(state);
+    },
+    upDateNewPostText (newText) {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state);
+    },
+    subscribe (observer) {
+        rerenderEntireTree = observer;
     }
-};
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export const upDateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
