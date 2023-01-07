967fb47707eabdca6e91ee26e50bc8dcb48a073e
Artyom <artem.sok27@gmail.com>
2023-01-07 21:57:15 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -1,11 +1,10 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { addPost, upDateNewPostText } from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,14 +13,14 @@ let rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} addPost={addPost} upDateNewPostText={upDateNewPostText} />
+        <App state={store.getState()} addPost={store.addPost} upDateNewPostText={store.upDateNewPostText} />
       </BrowserRouter>
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store._state);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
