7fabb191e63d58c0fab9334953a1b10ba540870e
Artyom <artem.sok27@gmail.com>
2023-01-08 12:05:51 +0300

    dispatch

--- social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ social network/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -14,7 +14,8 @@ const MyPosts = (props) => {
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch ({type: 'UPDATE-NEW-POST-TEXT'});
+    let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+    props.dispatch (action);
   }
 
   return (
