3669c28a20d7204bd32428b70b4b1e8a445c86fb
Artyom <artem.sok27@gmail.com>
2023-01-08 16:37:25 +0300

    commit

--- social network/my-app/src/redux/state.js
+++ social network/my-app/src/redux/state.js
@@ -1,5 +1,7 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 
 let store = {
     _state: {
@@ -25,8 +27,9 @@ let store = {
                 { id: 3, message: "Yo" },
                 { id: 4, message: "Yo" },
                 { id: 5, message: "Yo" },
-            ]
-        }
+            ],
+            newMessageBody: ""
+        },
     },
     _callSubscriber() {
         console.log('State')
@@ -53,14 +56,25 @@ let store = {
         } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+            this._state.dialogsPage.profilePage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE) {
+            let body = this._state.dialogsPage.profilePage.newMessageBody;
+            this._state.dialogsPage.profilePage.newMessageBody = "";
+            this._state.dialogsPage.messages.push({ id: 6, message: body });
+            this._callSubscriber(this._state);
         }
     }
 }
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
-
 export const upDateNewPostTextActionCreator = (text) =>
     ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
+export const updateNewMessageBodyCreator = (body) =>
+    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
 
 export default store;
 window.store = store;
