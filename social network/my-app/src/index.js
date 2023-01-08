29c264738aa1758538109e6a144a37bc1525653b
Artyom <artem.sok27@gmail.com>
2023-01-08 17:21:09 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -13,7 +13,7 @@ let rerenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} dispatch={store.dispatch.bind(store)} />
+        <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
       </BrowserRouter>
     </React.StrictMode>
   );
