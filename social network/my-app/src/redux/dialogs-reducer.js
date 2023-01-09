e0256b3c63b48e5a9cc1f3ca73fadd6e3c81d045
Artyom <artem.sok27@gmail.com>
2023-01-09 20:41:49 +0300

    switch

--- social network/my-app/src/redux/dialogs-reducer.js
+++ social network/my-app/src/redux/dialogs-reducer.js
@@ -2,16 +2,18 @@ const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
 const dialogsReducer = (state, action) => {
-    
-    if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        state.newMessageBody = action.body;
-    } else if (action.type === SEND_MESSAGE) {
-        let body = state.newMessageBody;
-        state.newMessageBody = "";
-        state.messages.push({ id: 6, message: body });
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            state.newMessageBody = "";
+            state.messages.push({ id: 6, message: body });
+            return state;
+        default:
+            return state;
     }
-    
-    return state;
 }
 
 export default dialogsReducer;
