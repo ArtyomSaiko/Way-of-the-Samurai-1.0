12c731524db7789c53603e9646341e7af33c03af
Artyom <artem.sok27@gmail.com>
2023-01-09 20:42:51 +0300

    commit

--- social network/my-app/src/redux/state.js
+++ social network/my-app/src/redux/state.js
@@ -2,11 +2,6 @@ import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND-MESSAGE';
-
 let store = {
     _state: {
         profilePage: {
