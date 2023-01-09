49ff717b5451a378e5e5dfe80c8724f8cf978eff
Artyom <artem.sok27@gmail.com>
2023-01-09 20:41:58 +0300

    switch

--- social network/my-app/src/redux/profile-reducer.js
+++ social network/my-app/src/redux/profile-reducer.js
@@ -2,19 +2,21 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
-    
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
-    } 
-    return state;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 0,
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
+    }
 }
-
 export default profileReducer;
