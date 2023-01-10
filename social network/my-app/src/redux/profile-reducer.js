ee395675a896a36e79170e903d843a9257efa7f4
Artyom <artem.sok27@gmail.com>
2023-01-10 22:24:14 +0300

    commit

--- social network/my-app/src/redux/profile-reducer.js
+++ social network/my-app/src/redux/profile-reducer.js
@@ -2,7 +2,9 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
+
     switch (action.type) {
+
         case ADD_POST:
             let newPost = {
                 id: 5,
@@ -12,9 +14,11 @@ const profileReducer = (state, action) => {
             state.posts.push(newPost);
             state.newPostText = '';
             return state;
+
         case UPDATE_NEW_POST_TEXT:
             state.newPostText = action.newText;
             return state;
+            
         default:
             return state;
     }
