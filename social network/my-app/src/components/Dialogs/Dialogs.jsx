fb01c7faff46b7400d1c45919fbfaa9da41aa0b6
Artyom <artem.sok27@gmail.com>
2023-01-10 22:23:58 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,6 @@ import Message from './Message/Message';
 const Dialogs = (props) => {
 
     let state = props.store.getState().dialogsPage;
-
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id} />);
     let newMessageBody = state.newMessageBody;
@@ -15,7 +14,6 @@ const Dialogs = (props) => {
     let onSendMessegeClick = () => {
         props.store.dispatch(sendMessageActionCreator());
     }
-
     let onNewMessageChange = (e) => {
         let body = e.target.value;
         props.store.dispatch(upDateNewMessageBodyActionCreator(body));
