8817a3443e6e95fbe44c2ae3a2bfd8c7062f0701
Artyom <artem.sok27@gmail.com>
2023-01-12 20:42:34 +0300

    commit

--- social network/my-app/src/redux/redux-store.js
+++ social network/my-app/src/redux/redux-store.js
@@ -1,16 +1,14 @@
-import {combineReducers, legacy_createStore as createStore} from "redux";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
-
-let reducers = combineReducers ({
-    profileReducer: profileReducer,
-    dialogsReducer: dialogsReducer,
-    sidebarReducer: sidebarReducer
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer
 });
 
-
-let store = createStore (reducers);
+let store = createStore(reducers);
 
 export default store;
