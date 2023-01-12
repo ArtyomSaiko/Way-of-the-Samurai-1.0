2749af284775de9b68686f5f54b2808bc2d99436
Artyom <artem.sok27@gmail.com>
2023-01-12 20:18:10 +0300

    commit

--- social network/my-app/src/redux/dialogs-reducer.js
+++ social network/my-app/src/redux/dialogs-reducer.js
@@ -1,7 +1,26 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        { id: 1, name: 'Andrey' },
+        { id: 2, name: 'Svetay' },
+        { id: 3, name: 'Sasha' },
+        { id: 4, name: 'Dimych' },
+        { id: 5, name: 'Victo' },
+        { id: 6, name: 'Valera' },
+    ],
+    messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How is you" },
+        { id: 3, message: "Yo" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+    ],
+    newMessageBody: ""
+};
+
+const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
