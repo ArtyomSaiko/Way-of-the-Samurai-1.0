db1d7af8535cbf9db73be2fcf686399fcc440c6f
Artyom <artem.sok27@gmail.com>
2023-01-14 18:39:01 +0300

    commit

--- social network/my-app/src/redux/dialogs-reducer.js
+++ social network/my-app/src/redux/dialogs-reducer.js
@@ -21,18 +21,15 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
-
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
             return state;
-
         case SEND_MESSAGE:
             let body = state.newMessageBody;
             state.newMessageBody = "";
             state.messages.push({ id: 6, message: body });
             return state;
-
         default:
             return state;
     }
