019791ab8c6c3c51e6f7981feb920fc9bfcd4d91
Artyom <artem.sok27@gmail.com>
2023-01-14 18:39:13 +0300

    commit

--- social network/my-app/src/redux/redux-store.js
+++ social network/my-app/src/redux/redux-store.js
@@ -3,7 +3,7 @@ import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
-let reducers = combineReducers({
+let reducers = combineReducers ({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer
