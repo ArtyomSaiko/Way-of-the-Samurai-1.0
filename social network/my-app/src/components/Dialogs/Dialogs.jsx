096cedcb18996c84d0f17ae369767e9a370a109b
Artyom <artem.sok27@gmail.com>
2023-01-15 16:24:54 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -7,6 +7,7 @@ import Message from './Message/Message';
 const Dialogs = (props) => {
 
     let state = props.store.getState().dialogsPage;
+    
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id} />);
     let newMessageBody = state.newMessageBody;
