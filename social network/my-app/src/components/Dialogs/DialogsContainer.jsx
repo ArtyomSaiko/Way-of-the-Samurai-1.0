b558b2ef0c4f5238c094e1acb5e4c0849d7358f4
Artyom <artem.sok27@gmail.com>
2023-01-15 17:32:27 +0300

    DialogsContainer

---
+++ social network/my-app/src/components/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { sendMessageActionCreator, upDateNewMessageBodyActionCreator, } from '../../redux/dialogs-reducer';
+import Dialogs from './Dialogs';
+import s from './Dialogs.module.css';
+
+
+const DialogsContainer = (props) => {
+
+    let state = props.store.getState().dialogsPage;
+
+    let onSendMessegeClick = () => {
+        props.store.dispatch(sendMessageActionCreator());
+    }
+    let onNewMessageChange = (body) => {
+        props.store.dispatch(upDateNewMessageBodyActionCreator(body));
+    }
+
+    return <Dialogs upDateNewMessageBody={onNewMessageChange}
+        sendMessage={onSendMessegeClick}
+        dialogsPage={state} />
+}
+
+export default DialogsContainer;
