2a8d03055720b170c5be66e22dc1e15d2953ec34
Artyom <artem.sok27@gmail.com>
2023-01-15 17:32:33 +0300

    DialogsContainer

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import { sendMessageActionCreator, upDateNewMessageBodyActionCreator, } from '../../redux/dialogs-reducer';
 import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let state = props.store.getState().dialogsPage;
-    
+    let state = props.dialogsPage;
+
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id} />);
     let newMessageBody = state.newMessageBody;
 
     let onSendMessegeClick = () => {
-        props.store.dispatch(sendMessageActionCreator());
+        props.sendMessage();
     }
     let onNewMessageChange = (e) => {
         let body = e.target.value;
-        props.store.dispatch(upDateNewMessageBodyActionCreator(body));
+        props.upDateNewMessageBody(body);
     }
 
     return (
