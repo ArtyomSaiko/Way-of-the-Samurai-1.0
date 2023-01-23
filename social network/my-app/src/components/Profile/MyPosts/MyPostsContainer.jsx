52de96c4e1bfa4757a0a9ee535952a0c3ba26fc2
Artyom <artem.sok27@gmail.com>
2023-01-23 22:11:59 +0300

    commit

--- social network/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ social network/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { addPostActionCreator, upDateNewPostTextActionCreator } from '../../../redux/profile-reducer';
+import StoreContext from '../../../StoreContext';
 import MyPosts from './MyPosts';
 
-const MyPostsContainer = (props) => {
-  let state = props.store.getState();
-
-  let addPost = () => {
-    props.store.dispatch(addPostActionCreator());
-  }
-  let onPostChange = (text) => {
-    let action = upDateNewPostTextActionCreator(text)
-    props.store.dispatch(action);
-  }
+const MyPostsContainer = () => {
 
   return (
-    <StoreComtext.Consumer> {
-      (store) => (
-      <MyPosts upDateNewPostText={onPostChange}
-        addPost={addPost}
-        posts={state.profilePage.posts}
-        newPostText={state.profilePage.newPostText} />
-    )}
-    </StoreComtext.Consumer>
+    <StoreContext.Consumer> {
+      (store) => {
+        let state = store.getState();
+        let addPost = () => {
+          store.dispatch(addPostActionCreator());
+        }
+
+        let onPostChange = (text) => {
+          let action = upDateNewPostTextActionCreator(text)
+          store.dispatch(action);
+        }
+
+        return <MyPosts upDateNewPostText={onPostChange}
+          addPost={addPost}
+          posts={store.getState().profilePage.posts}
+          newPostText={store.getState().profilePage.newPostText} />
+      }
+    }
+    </StoreContext.Consumer>
   )
 }
 
