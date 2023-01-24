4c2cac1e877ffbad31ec03542a7bb1279b8eb7c0
Artyom <artem.sok27@gmail.com>
2023-01-24 22:08:48 +0300

    commit

--- social network/my-app/src/index.js
+++ social network/my-app/src/index.js
@@ -24,10 +24,6 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
