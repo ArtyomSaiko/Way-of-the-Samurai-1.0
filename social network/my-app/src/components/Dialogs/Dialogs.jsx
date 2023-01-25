9a44e9f590ee2489b3e4d3aeec21356939b77959
Artyom <artem.sok27@gmail.com>
2023-01-25 22:34:36 +0300

    commit

--- social network/my-app/src/components/Dialogs/Dialogs.jsx
+++ social network/my-app/src/components/Dialogs/Dialogs.jsx
@@ -7,8 +7,8 @@ const Dialogs = (props) => {
 
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
-    let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id} />);
+    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id} />);
+    let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id} id={m.id} />);
     let newMessageBody = state.newMessageBody;
 
     let onSendMessegeClick = () => {
