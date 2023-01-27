6dc6268504ee5669e19c53cf64997347f3af9e95
Artyom <artem.sok27@gmail.com>
2023-01-27 23:03:41 +0300

    commit

--- social network/my-app/src/components/Users/Users.jsx
+++ social network/my-app/src/components/Users/Users.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import styles from './Users.module.css';
 
 let Users = (props) => {
     return <div>
-        USERS WILL BE HERE
+        {
+            props.users.map( u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photoUrl} className ={styles.userPhoto} />
+                    </div>
+                    <div>
+                        <button> FOLLOW </button>
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.fullName}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.country}</div>
+                        <div>{u.location.city}</div>
+                    </span>
+                </span>
+            </div>)
+        }
     </div>
 }
 
