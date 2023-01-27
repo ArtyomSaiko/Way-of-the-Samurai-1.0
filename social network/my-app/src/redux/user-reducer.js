dbe47f8b94cad04d579262551bd2fb6f289adf36
Artyom <artem.sok27@gmail.com>
2023-01-27 22:21:42 +0300

    SET_USERS

--- social network/my-app/src/redux/user-reducer.js
+++ social network/my-app/src/redux/user-reducer.js
@@ -16,22 +16,27 @@ const userReducer = (state = initialState, action) => {
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if(u.id === action.userId) {
-                        return {...u, followed: true}
+                    if (u.id === action.userId) {
+                        return { ...u, followed: true }
                     }
                     return u;
                 })
             }
+
         case UNFOLLOW:
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if(u.id === action.userId) {
-                        return {...u, followed: false}
+                    if (u.id === action.userId) {
+                        return { ...u, followed: false }
                     }
                     return u;
                 })
             }
+
+        case SET_USERS:
+            return { ...state, users: [...state.users, ...action.users]}
+
         default:
             return state;
     }
