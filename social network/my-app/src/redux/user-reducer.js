455afefeb17dc2558a93c41eddeb4a56d7f9f03e
Artyom <artem.sok27@gmail.com>
2023-01-27 22:15:48 +0300

    commit

--- social network/my-app/src/redux/user-reducer.js
+++ social network/my-app/src/redux/user-reducer.js
@@ -3,25 +3,41 @@ const UNFOLLOW = 'UNFOLLOW';
 
 let initialState = {
     users: [
-        { id: 1, followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', cuontry: 'Belsrus'}},
-        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', cuontry: 'Russia'}},
-        { id: 3, followed: false, fullName: 'Andrey', status: 'I am a boss too', location: {city: 'Kiev', cuontry: 'Ukraine'}}
+        { id: 1, followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: { city: 'Minsk', cuontry: 'Belsrus' } },
+        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', cuontry: 'Russia' } },
+        { id: 3, followed: false, fullName: 'Andrey', status: 'I am a boss too', location: { city: 'Kiev', cuontry: 'Ukraine' } }
     ]
 };
 
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case FOLLOW:
-
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if(u.id === action.userId) {
+                        return {...u, followed: true}
+                    }
+                    return u;
+                })
+            }
         case UNFOLLOW:
-
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if(u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+            }
         default:
             return state;
     }
 }
 
-export const followAC = (userId) => ({ type: FOLLOW, userId})
+export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) =>
-    ({ type: UNFOLLOW, userId})
+    ({ type: UNFOLLOW, userId })
 
 export default userReducer;
