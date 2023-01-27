55e1c15b8b50c62e395fc13b714d9f51eef00887
Artyom <artem.sok27@gmail.com>
2023-01-27 22:03:49 +0300

    false/true

--- social network/my-app/src/redux/user-reducer.js
+++ social network/my-app/src/redux/user-reducer.js
@@ -3,11 +3,10 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
     users: [
-        { id: 1, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', cuontry: 'Belsrus'}},
-        { id: 2, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', cuontry: 'Russia'}},
-        { id: 3, fullName: 'Andrey', status: 'I am a boss too', location: {city: 'Kiev', cuontry: 'Ukraine'}}
-    ],
-    newPostText: 'it-kamasytra'
+        { id: 1, followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', cuontry: 'Belsrus'}},
+        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', cuontry: 'Russia'}},
+        { id: 3, followed: false, fullName: 'Andrey', status: 'I am a boss too', location: {city: 'Kiev', cuontry: 'Ukraine'}}
+    ]
 };
 
 const userReducer = (state = initialState, action) => {
