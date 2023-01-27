8bae1e7ce4d927264cbfa566a766ed2bf05ab2df
Artyom <artem.sok27@gmail.com>
2023-01-27 23:03:50 +0300

    commit

--- social network/my-app/src/redux/users-reducer.js
+++ social network/my-app/src/redux/users-reducer.js
@@ -4,10 +4,10 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        /*{ id: 1, followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: { city: 'Minsk', cuontry: 'Belsrus' } },
-        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', cuontry: 'Russia' } },
-        { id: 3, followed: false, fullName: 'Andrey', status: 'I am a boss too', location: { city: 'Kiev', cuontry: 'Ukraine' } }
-    */ ]
+        { id: 1, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg', followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: { city: 'Minsk', cuontry: 'Belsrus' } },
+        { id: 2, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', cuontry: 'Russia' } },
+        { id: 3, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg', followed: false, fullName: 'Andrey', status: 'I am a boss too', location: { city: 'Kiev', cuontry: 'Ukraine' } }
+    ]
 };
 
 const userReducer = (state = initialState, action) => {
