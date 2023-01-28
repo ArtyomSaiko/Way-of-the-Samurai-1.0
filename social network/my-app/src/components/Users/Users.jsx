21d5542117535a876dc735ffec9bae4a7a865fd4
Artyom <artem.sok27@gmail.com>
2023-01-28 21:11:37 +0300

    commit

--- social network/my-app/src/components/Users/Users.jsx
+++ social network/my-app/src/components/Users/Users.jsx
@@ -5,40 +5,45 @@ let Users = (props) => {
 
     if (props.users.length === 0) {
         props.setUsers([
-            {id: 1, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
-                followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: { city: 'Minsk', cuontry: 'Belarus' }},
-            {id: 2, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
-                followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', cuontry: 'Russia' }},
-            {id: 3, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
-                followed: false, fullName: 'Andrey', status: 'I am a boss too', location: { city: 'Kiev', cuontry: 'Ukraine' }}
-            ]
+            {
+                id: 1, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
+                followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: { city: 'Minsk', cuontry: 'Belarus' }
+            },
+            {
+                id: 2, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
+                followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', cuontry: 'Russia' }
+            },
+            {
+                id: 3, photoUrl: 'https://instagramis.ru/wp-content/uploads/dimas-blog.jpg',
+                followed: false, fullName: 'Andrey', status: 'I am a boss too', location: { city: 'Kiev', cuontry: 'Ukraine' }
+            }
+        ]
         )
     }
 
     return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+        {props.users.map(u => <div key={u.id}>
+            <span>
+                <div>
+                    <img src={u.photoUrl} className={styles.userPhoto} />
+                </div>
+                <div>
+                    {u.followed
+                        ? <button onClick={() => { props.unfollow(u.id) }}> UNFOLLOW </button>
+                        : <button onClick={() => { props.follow(u.id) }}> FOLLOW </button>}
+                </div>
+            </span>
+            <span>
                 <span>
-                    <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }}> UNFOLLOW </button>
-                            : <button onClick={() => { props.follow(u.id) }}> FOLLOW </button>}
-                    </div>
+                    <div>{u.fullName}</div>
+                    <div>{u.status}</div>
                 </span>
                 <span>
-                    <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
-                    </span>
+                    <div>{u.location.country}</div>
+                    <div>{u.location.city}</div>
                 </span>
-            </div>)
+            </span>
+        </div>)
         }
     </div>
 }
