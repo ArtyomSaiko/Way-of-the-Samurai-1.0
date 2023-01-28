42188e1846d7d517822fca01204ddeae1c11b793
Artyom <artem.sok27@gmail.com>
2023-01-28 22:10:08 +0300

    axios

--- social network/my-app/src/components/Users/Users.jsx
+++ social network/my-app/src/components/Users/Users.jsx
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import React from 'react';
 import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
-
     if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers()   
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then( response => {
+            props.setUsers(response.data.items)   
         });
-        
     }
 
     return <div>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto} />
+                    <img src={u.photos.small != null ? u.photos.small: userPhoto} className={styles.userPhoto} />
                 </div>
                 <div>
                     {u.followed
@@ -29,8 +29,8 @@ let Users = (props) => {
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
         </div>)
