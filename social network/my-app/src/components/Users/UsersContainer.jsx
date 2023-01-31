0aeeac2efc02613d1f160b1d6ab10dca2650da19
Artyom <artem.sok27@gmail.com>
2023-01-31 21:28:17 +0300

    commit

--- social network/my-app/src/components/Users/UsersContainer.jsx
+++ social network/my-app/src/components/Users/UsersContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { followAC, setUsersAC, unfollowAC } from '../../redux/users-reducer';
 
 let mapStateToProps = (state) => {
+    
     return {
         users: state.usersPage.users
     }
