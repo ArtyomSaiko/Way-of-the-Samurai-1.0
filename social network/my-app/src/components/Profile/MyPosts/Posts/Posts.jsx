0fbb1cc036c152bc2bfd073cee8b302edc6f65a3
Artyom <artem.sok27@gmail.com>
2023-02-10 20:14:34 +0300

    commit

--- social network/my-app/src/components/Profile/MyPosts/Posts/Posts.jsx
+++ social network/my-app/src/components/Profile/MyPosts/Posts/Posts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Posts.module.css';
 
 const Posts = (props) => {
+  
   return (
     <div className={s.item}>
       <img src='https://shapka-youtube.ru/wp-content/uploads/2021/02/prikolnaya-avatarka-dlya-patsanov.jpg' />
