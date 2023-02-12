93eeaa813fb16562fd811122b7029c3dfa05ce13
Artyom <artem.sok27@gmail.com>
2023-02-12 20:44:59 +0300

    commit

--- social network/my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ social network/my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -5,6 +5,7 @@ import s from './../Dialogs.module.css';
 const DialogItem = (props) => {
 
   let path = "/dialogs/" + props.id;
+
   return (
     <div className={s.dialog + ' ' + s.active}>
       <NavLink to={path}> {props.name} </NavLink>
